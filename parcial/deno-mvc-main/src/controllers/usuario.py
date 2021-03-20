606752de6e0aa7f546e911bc764dc47c5958d797
Ionatan <jhonatanjaramillo8@gmail.com>
2021-03-19 20:43:35 -0500

    entregaparcial

---
+++ parcial/deno-mvc-main/src/controllers/usuario.py
@@ -0,0 +1,50 @@
+from models.user import UsuariosModel
+from flask import Flask, jsonify, request
+
+class UserControl():
+
+    def listado(self):
+
+        usuarioslis =UsuariosModel()
+        usuarios = usuarioslis.listar()
+        return (usuarios)  
+   
+    def crear(self, nombre, apellido, celular, correo, contrasenia):
+ 
+        usuariocrear =UsuariosModel()
+        usuariocrear.crear(nombre,apellido,celular,correo,contrasenia)
+    
+    def actualizar(self, nombre, apellido, celular, correo, contrasenia,cod):
+ 
+        usuarioactualizar =UsuariosModel()
+        usuarioactualizar.actualizar(nombre,apellido,celular,correo,contrasenia,cod)
+
+    def borrar(self, cod):
+     
+        usuarioborrar =UsuariosModel()
+        usuarioborrar.eliminar(cod)
+
+
+    def listarbd(self):
+
+        user = UsuariosModel()
+        databases = user.mostrardatabase()
+        return (databases)
+
+    def creardatabase(self, name):
+
+        name = request.json['name']
+        user = UsuariosModel()
+        user.creardatabase(name)
+        return 'hecho'
+
+    def mostrartablas(self):
+
+        user = UsuariosModel()
+        mostrartablas = user.mostrartablas()
+        return (mostrartablas)
+    
+    def editartabla(self,nombretabla, nombrecolumna, descripcion,tipodato):
+ 
+        editartabla =UsuariosModel()
+        editartabla.alterartabla(nombretabla, nombrecolumna,descripcion,tipodato)
---
+++ parcial/deno-mvc-main/src/models/user.py
@@ -0,0 +1,75 @@
+from config.mariadb import DB
+
+import mariadb
+
+class UsuariosModel():
+    
+    def listar(self):
+
+        cursor = DB.cursor()
+        cursor.execute('select * from datosuser ')
+        usuarios = cursor.fetchall()
+        cursor.close()
+        return usuarios
+
+    def crear(self, nombre, apellido, celular, correo, contrasenia):
+        
+        cursor = DB.cursor()
+        cursor.execute('insert into datosuser(nombre,apellido,celular,correo,contrasenia) values(?,?,?,?,?)', 
+        (nombre,apellido,celular,correo,contrasenia,))
+        cursor.close()
+
+     
+    def eliminar(self, cod):
+
+        cursor = DB.cursor()
+        cursor.execute('delete from datosuser WHERE id = ? ', cod)
+        cursor.close()
+
+        
+    def actualizar(self,nombre,apellido,celular,correo,contrasenia,cod):
+
+        cursor = DB.cursor()
+        cursor.execute('update datosuser set nombre = ?, apellido = ?, celular = ?, correo = ?, contrasenia = ? WHERE id = ? ', 
+        (nombre,apellido,celular,correo,contrasenia,cod))
+        cursor.close()
+
+    def mostrardatabase (self):
+
+        cursor = DB.cursor()
+        cursor.execute('SHOW DATABASES')
+        databases = cursor.fetchall()
+        cursor.close()
+        return databases
+
+    def creardatabase (self, name):
+
+        cursor = DB.cursor()
+        cursor.execute('CREATE DATABASE '+ name)
+        cursor.close()
+
+    def mostrartablas (self):
+
+        cursor = DB.cursor()
+        cursor.execute('SHOW FULL TABLES FROM user ')
+        mistablas = cursor.fetchall()
+        cursor.close()
+        return mistablas 
+
+    def creartabla (self):
+
+        cursor = DB.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS nombre_tabla (definición de la tabla,definición de columnas,tipos de columnas;)')
+        mistablas = cursor.fetchall()
+        cursor.close()
+        return mistablas
+
+    def alterartabla (self,nombretabla,nombrecolumna,descripcion,tipodato):
+
+        cursor = DB.cursor()
+        cursor.execute('ALTER TABLE '+nombretabla+' CHANGE '+nombrecolumna+' '+descripcion+' '+tipodato+';')
+        cursor.close()
+        
+
+
+        
